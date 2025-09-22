fb50c73e4b681a2250460a0a8681da7ce14ed93c
Da-shou <fazlijaylli@gmail.com>
2025-09-22 02:52:40 +0200

    Small changes to building scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # Compile project using CMake
 cmake --build . --target clean
-cmake -S . -B build
+cmake -S .
 cmake --build .
 
 # Run program if successfully compiled
-if [ -f ./da_renderer ]; then
-	./da_renderer
+if [ -f ./build/da_renderer ]; then
+	./build/da_renderer
 fi
